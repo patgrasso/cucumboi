4045875ac7f5061025a986a5af12aeb596722b0f
Nicholas Zubrycki <nzubrycki@gmail.com>
2015-04-24 15:30:41 -0400

    edited commments again

--- js/inc/game-state.js
+++ js/inc/game-state.js
@@ -25,40 +25,33 @@ define(['Phaser'], function (Phaser) {
 
         score = 0;
 
-        //  We're going to be using physics, so enable the Arcade Physics system
+        // enable the Arcade Physics system
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        //  A simple background for our game
+        // background
         this.game.add.sprite(0, 0, 'sky');
 
-        //  The platforms group contains the ground and the 2 ledges we can jump on
+        // platforms group contains the ground and the 2 ledges we can jump on
         platforms = this.game.add.group();
-        //  We will enable physics for any object that is created in this group
+        // enable physics for any object that is created in this group
         platforms.enableBody = true;
 
-        // Here we create the ground.
+        // ground
         ground = platforms.create(0, this.game.world.height - 64, 'ground');
-
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
         ground.scale.setTo(2, 2);
-
-        //  This stops it from falling away when you jump on it
         ground.body.immovable = true;
 
-        //  Now let's create two ledges
+        // two ledges
         ledge = platforms.create(400, 400, 'ground');
         ledge.body.immovable = true;
-
         ledge = platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
 
-        // The player and its setting
+        // player
         player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
-
-        //  We need to enable physics on the player
+        // enable physics on the player
         this.game.physics.arcade.enable(player);
-
-        //  Player physics properties.
+        // player physics properties
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
 
@@ -66,29 +59,24 @@ define(['Phaser'], function (Phaser) {
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-		// Initialize the score text
+		// initialize the score text
         scoreText = this.game.add.text(16, 16, 'score: 0', {
             fontSize: '32px',
             fill: '#000'
         });
 
-        //  Our controls.
+        // controls
         cursors = this.game.input.keyboard.createCursorKeys();
 
         stars = this.game.add.group();
-
         stars.enableBody = true;
 
-        //  Here we'll create 12 of them evenly spaced apart
+        // create 12 of them evenly spaced apart
         for (var i = 0; i < 12; i++)
         {
-            //  Create a star inside of the 'stars' group
+            // create a star inside of the 'stars' group
             var star = stars.create(i * 70, 0, 'star');
-
-            //  Let gravity do its thing
-            star.body.gravity.y = 6;
-
-            //  This just gives each star a slightly random bounce value
+            star.body.gravity.y = 500;
             star.body.bounce.y = 0.7 + Math.random() * 0.2;
         }
     };
@@ -99,30 +87,29 @@ define(['Phaser'], function (Phaser) {
      */
     gameState.prototype.update = function () {
 
-        //  Collide the player and the stars with the platforms
+        //  collide the player and the stars with the platforms
         this.game.physics.arcade.collide(player, platforms);
         this.game.physics.arcade.collide(stars, platforms);
 
-		// Allows the player to collect stars and have them get removed from the screen
+		// allows the player to collect stars and have them get removed from the screen
         this.game.physics.arcade.overlap(stars, player, collectStar);
 
-        //  Reset the players velocity (movement)
+        //  reset the players velocity (movement)
         player.body.velocity.x = 0;
 
         if (cursors.left.isDown) {
             //  Move to the left
             player.body.velocity.x = -150;
-
             player.animations.play('left');
+			
         } else if (cursors.right.isDown) {
             //  Move to the right
             player.body.velocity.x = 150;
-
             player.animations.play('right');
+			
         } else {
             //  Stand still
             player.animations.stop();
-
             player.frame = 4;
         }
 
